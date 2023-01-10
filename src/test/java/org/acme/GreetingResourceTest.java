bb7a4cf25843e1e02b1910efb183095b171d9091
enrico84 <enrico8484@gmail.com>
2023-01-10 12:17:47 +0100

    Add Unit tests and add finalName in the pom.xml

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -2,7 +2,9 @@ package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
+import javax.inject.Inject;
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
@@ -10,6 +12,9 @@ import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class GreetingResourceTest {
+
+    @Inject
+    GreetingService greetingService;
 
     @Test
     public void testHelloEndpoint() {
@@ -29,6 +34,14 @@ public class GreetingResourceTest {
                 .then()
                 .statusCode(200)
                 .body(is("Hello " + uuid));
+    }
+
+    @Test
+    public void testGreetingService() {
+        String name = "Enrico";
+        var output = greetingService.greeting(name);
+        System.out.println(output);
+        Assertions.assertTrue(output.contains("Hello "+name));
     }
 
 }
